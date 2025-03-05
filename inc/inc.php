42fff113d858d5c4448d9aca46ac892779f2b94a
alexshack <alexshack@yandex.ru>
2025-03-05 23:25:23 +0300

    initial commit

---
+++ inc/inc.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'uon/uon.php';
+
+echo '<pre>';
+print_r(Uon::uonUserOrders(2));
+echo '</pre>';
---
+++ inc/uon/uon.php
@@ -0,0 +1,136 @@
+<?php
+
+class Uon {
+
+    static $key = '41HtbHEOuN3xp8Ti8l021740391076';
+    static $main_url = 'https://api.u-on.ru/';
+    static $url = '';
+
+    static function init() {
+        self::$url = self::$main_url . self::$key . '/';
+    }
+
+    static function uonUserByEmail($email) {
+        $url = self::$url . 'user/email.json';
+        $data = array(
+            'email' => $email
+        );
+        $result = wp_remote_post( $url, ['body' => $data]);
+        $data = json_decode($result['body'], false);
+        if (is_array($data)) {
+            return $data[0];
+        }
+        return false;
+    }
+
+    static function uonUserOrders($user_id) {
+        $user = get_user_by('id', $user_id);
+        $uon_id = get_user_meta($user->ID, 'uon_id', true);
+        $url = self::$url . 'request/search.json';
+        $data = array(
+            'client_ids' => $uon_id
+        );
+        $result = wp_remote_post( $url, ['body' => $data]);
+        $data = json_decode($result['body'], false);
+        if (isset($data->requests)) {
+            return $data->requests;
+        }
+        return false;
+    }
+
+    static function wpUsers($uon = true) {
+        if ($uon) {
+            $args = array(
+                'role' => 'contributor',
+                'meta_query' => [
+                    'relation' => 'AND',
+                    [
+                        'key' => 'uon_id',
+                        'compare' => 'EXISTS',
+                    ],
+                    [
+                        'key' => 'uon_id',
+                        'compare' => '!=',
+                        'value' => '',
+                    ],
+                ],
+            );
+
+        } else {
+            $args = array(
+                'role' => 'contributor',
+                'meta_query' => [
+                    'relation' => 'OR',
+                    [
+                        'key' => 'uon_id',
+                        'compare' => 'NOT EXISTS',
+                    ],
+                    [
+                        'key' => 'uon_id',
+                        'value' => '',
+                    ],
+                ],
+            );
+        }
+        return get_users($args);
+    }
+
+    static function wpOrder($uon_id) {
+        $order = new WP_Query([
+            'post_type' => 'orders',
+            'meta_query' => [
+                'relation' => 'AND',
+                [
+                    'key' => 'uon_id',
+                    'value' => $uon_id,
+                ],
+            ],
+        ]);
+        if ($order->have_posts()) {
+            return $order->posts[0];
+        }
+        return false;
+    }
+    static function wpSetOrder($order) {
+        $wp_order = self::wpOrder($order->id);
+        if ($wp_order) {
+
+        } else {
+
+            $order_args = array(
+                'post_type' => 'orders',
+                'post_title' => $order->id,
+                'post_status' => 'publish',
+                'meta_input' => [
+                    'uon_id' => $order->id,
+                ],
+            );
+            $wp_order = wp_insert_post($order_args);
+        }
+
+    }
+
+    static function updateAll() {
+
+        //Получаем u_id для всех пользователей
+        $users = self::wpUsers(false);
+        foreach ($users as $user) {
+            $uon_data = self::uonUserByEmail($user->user_email);
+            if (isset($uon_data->u_id)) {
+                update_user_meta( $user->ID, 'uon_id', $uon_data->u_id );
+            }
+        }
+
+        //Обновляем все заказы
+        $users = self::wpUsers();
+        foreach ($users as $user) {
+            $u_data = self::uonUserOrders($user->ID);
+            foreach ($u_data as $order) {
+                self::wpSetOrder($order);
+            }
+        }
+
+    }
+
+}
+Uon::init();
